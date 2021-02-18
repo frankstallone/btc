1bb0c3bd89e51478bc046d1344b1795ee6ee9cb3
Frank Stallone III <frank@stallone.dev>
2021-02-18 15:18:20 -0500

    config: 🔧 Adding vendor and public folders, and updating view.php to point to new storage folder

--- wp-content/themes/bct/config/view.php
+++ wp-content/themes/bct/config/view.php
@@ -28,7 +28,7 @@ return [
     |
     */
 
-    'compiled' => get_theme_file_path('/storage/framework/views'),
+    'compiled' => wp_upload_dir()['basedir'].'/storage/framework/views',
 
     /*
     |--------------------------------------------------------------------------
