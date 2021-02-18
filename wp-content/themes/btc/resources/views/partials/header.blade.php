f1c46ca7d1655f7ba2e1036f3268afb956d74b9e
Frank Stallone III <frank@stallone.dev>
2021-02-18 18:14:12 -0500

    config: 🔧 Updating Tailwind config

--- wp-content/themes/btc/resources/views/partials/header.blade.php
+++ wp-content/themes/btc/resources/views/partials/header.blade.php
@@ -1,9 +1,9 @@
-<header class="banner">
-  <a class="brand" href="{{ home_url('/') }}">
+<header class="banner px-4 py-4 sm:px-6 lg:px-8 flex">
+  <a class="brand pr-4" href="{{ home_url('/') }}">
     {{ $siteName }}
   </a>
 
-  <nav class="nav-primary">
+  <nav class="nav-primary pl-4">
     @if (has_nav_menu('primary_navigation'))
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
     @endif
