5aadaf1afeec97cfd76ca232c7fa28d3c4f8767b
Frank Stallone III <frank@stallone.dev>
2021-08-01 10:01:21 -0400

    feat: ✨ Updating theme-color meta tag for latest safari

--- wp-content/themes/btc/index.php
+++ wp-content/themes/btc/index.php
@@ -10,7 +10,7 @@
     <link rel="manifest" href="/site.webmanifest">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#010d25">
-    <meta name="theme-color" content="#ffffff">
+    <meta name="theme-color" content="#0d1119">
     <meta name="google-site-verification" content="FGN6mtXSeyHDNLiN_LSVV5VtUjcOziQbpJqT-bKtbx8">
     <?php wp_head(); ?>
   </head>
